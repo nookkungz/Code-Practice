657b6ce8fd94586819e64b0e06dc6fd606465804
nookkungz <nookkung321@gmail.com>
2024-07-31 14:56:37 +0700

    Day 21

---
+++ E-Labsheet/lab test mid/1.py
@@ -0,0 +1,5 @@
+x = float(input(""))
+if x - int(x) == 0 :
+    print(f"{x:.0f} is an integer.")
+else :
+    print(f"{x} is not an integer.")
---
+++ E-Labsheet/lab5/4.py
@@ -0,0 +1,6 @@
+n = int(input(""))
+c = str(input(""))
+cc = 1
+while cc != n+1:
+    print(cc*c)
+    cc += 1
