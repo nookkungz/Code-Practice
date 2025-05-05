9b404122a97dca2763a2527ac52584e94a2d6396
nookkungz <nookkung321@gmail.com>
2025-05-05 12:46:42 +0700

    Day 63

--- Java/HackerRank/H3.java
+++ Java/HackerRank/H3.java
@@ -17,5 +17,14 @@ public class H3 {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+        if (N%2 == 1) {
+            System.out.println("Weird");
+        } else if (N >= 2 && N <= 5 ){
+            System.out.println("Not Weird");
+        } else if (N >= 6 && N <= 20){
+            System.out.println("Weird");
+        } else if (N > 20){
+            System.out.println("Not Weird");
+        }
     }
 }
