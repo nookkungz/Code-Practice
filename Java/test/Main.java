663228778aed5c46bf0730e00f720e573216e995
nookkungz <nookkung321@gmail.com>
2025-05-10 13:49:28 +0700

    Day 65

---
+++ Java/test/Main.java
@@ -0,0 +1,12 @@
+package Java.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WE NEED TO \n HI");
+        System.out.println("");
+        System.out.println("LEARN JAVA");
+        System.out.println("");
+        System.out.println("AS QUICKLY AS POSSIBLE");    
+        System.out.printf(null, args)
+    }
+}
